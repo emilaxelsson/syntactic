bb623ad3a970c9ce59879333f24756f60737ade9
Emil Axelsson <emax@chalmers.se>
2014-03-11 07:06:16 +0100

    Improved documentation of TypeUniverse

--- src/Data/Syntactic/TypeUniverse.hs
+++ src/Data/Syntactic/TypeUniverse.hs
@@ -1,7 +1,31 @@
--- | Typed type reification, type reasoning and dynamic types
+-- | Type universes and dynamic types
 --
 -- This module uses Data Types à la Carte to provide open universes. A simpler closed implementation
 -- is found in @extras/TypeUniverseClosed.hs@.
+--
+-- Example 1 (dynamic types):
+--
+-- > type MyUniverse = IntType :+: BoolType
+-- >
+-- > hlist :: [Dynamic MyUniverse]
+-- > hlist = [toDyn True, toDyn (1 :: Int)]
+--
+-- > *Main> hlist
+-- > [True,1]
+--
+-- Note that if we were using "Data.Dynamic", it would just print
+--
+-- > [<<Bool>>,<<Int>>]
+--
+-- Example 2 (dynamically typed addition):
+--
+-- > addDyn :: (TypeEq ts ts, PWitness Num ts ts) => Dynamic ts -> Dynamic ts -> Maybe (Dynamic ts)
+-- > addDyn (Dyn ta a) (Dyn tb b) = do
+-- >     Dict <- typeEq ta tb
+-- >     Dict <- pwit pNum ta
+-- >     return (Dyn ta (a+b))
+--
+-- "Data.Dynamic" could only do this monomorphically, for one 'Num' type at a time.
 
 module Data.Syntactic.TypeUniverse
     ( module Data.Syntactic.TypeUniverse.TypeUniverse
