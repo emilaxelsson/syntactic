f2bd28afe690e9ed23cdf21ed155e6cb0ff8f6de
Emil Axelsson <emax@chalmers.se>
2014-04-11 07:59:56 +0200

    Added benchmark for Dynamic

--- benchmarks/MainBenchmark.hs
+++ benchmarks/MainBenchmark.hs
@@ -3,9 +3,11 @@ module Main where
 import qualified Normal
 import qualified WithArity
 import qualified JoiningTypes
+import qualified Dynamic
 
 main :: IO ()
 main = do
   Normal.main
   WithArity.main
   JoiningTypes.main
+  Dynamic.main
