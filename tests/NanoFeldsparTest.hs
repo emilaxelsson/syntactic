180bf04b6fb84717a8a770796fd10486ecfdc851
Emil Axelsson <emax@chalmers.se>
2014-04-23 17:13:49 +0200

    Added test of alpha-inequivalence

--- tests/NanoFeldsparTest.hs
+++ tests/NanoFeldsparTest.hs
@@ -47,20 +47,35 @@ mkGold_matMul = writeFile "tests/gold/matMul.txt" $ Nano.showAST Nano.matMul
 alphaRename :: ASTF Nano.FeldDomain a -> ASTF Nano.FeldDomain a
 alphaRename = mapAST rename
   where
+    rename :: Nano.FeldDomain a -> Nano.FeldDomain a
     rename s
         | Just (VarT t v :: BindingT Nano.FeldTypes sig) <- prj s = inj (VarT t (v+1))
         | Just (LamT t v :: BindingT Nano.FeldTypes sig) <- prj s = inj (LamT t (v+1))
         | otherwise = s
 
+badRename :: ASTF Nano.FeldDomain a -> ASTF Nano.FeldDomain a
+badRename = mapAST rename
+  where
+    rename :: Nano.FeldDomain a -> Nano.FeldDomain a
+    rename s
+        | Just (VarT t v :: BindingT Nano.FeldTypes sig) <- prj s = inj (VarT t (v+1))
+        | Just (LamT t v :: BindingT Nano.FeldTypes sig) <- prj s = inj (LamT t (v-1))
+        | otherwise = s
+
 prop_alphaEq a = alphaEq a (alphaRename a)
+
+prop_alphaEqBad a = alphaEq a (badRename a)
 
 tests = testGroup "TreeTests"
     [ goldenVsString "scProd tree" "tests/gold/scProd.txt" $ return $ fromString $ Nano.showAST Nano.scProd
     , goldenVsString "matMul tree" "tests/gold/matMul.txt" $ return $ fromString $ Nano.showAST Nano.matMul
-    , testProperty   "scProd eval"    prop_scProd
-    , testProperty   "matMul eval"    prop_matMul
-    , testProperty   "alphaEq scProd" (prop_alphaEq (desugar Nano.scProd))
-    , testProperty   "alphaEq matMul" (prop_alphaEq (desugar Nano.matMul))
+    , testProperty "scProd eval" prop_scProd
+    , testProperty "matMul eval" prop_matMul
+    , testProperty "alphaEq scProd"        (prop_alphaEq (desugar Nano.scProd))
+    , testProperty "alphaEq matMul"        (prop_alphaEq (desugar Nano.matMul))
+    , testProperty "alphaEq scProd matMul" (not (alphaEq (desugar Nano.scProd) (desugar Nano.matMul)))
+    , testProperty "alphaEqBad scProd"     (not (prop_alphaEqBad (desugar Nano.scProd)))
+    , testProperty "alphaEqBad matMul"     (not (prop_alphaEqBad (desugar Nano.matMul)))
     ]
 
 main = defaultMain tests
