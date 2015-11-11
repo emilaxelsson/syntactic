399f5359082a2a622bf4840eafd48f87479695e2
Emil Axelsson <emax@chalmers.se>
2015-11-11 10:40:26 +0100

    Example demonstrating interplay between tuples and sharing

--- examples/NanoFeldspar.hs
+++ examples/NanoFeldspar.hs
@@ -361,6 +361,14 @@ transpose a = indexed (length (a!0)) $ \k -> indexed (length a) $ \l -> a ! l ! 
 fib :: Data Int -> Data Int
 fib n = fst $ forLoop n (0,1) $ \_ (a,b) -> (b,a+b)
 
+-- | Demonstration of how tuples interplay with sharing. Tuples are essentially
+-- useless without sharing. This function would get two identical for loops if
+-- it wasn't for sharing.
+sumOfMinMax :: Vector (Data Int) -> Data Int
+sumOfMinMax vec = lo+hi
+  where
+    (lo,hi) = fold (\a (l,h) -> (min a l, max a h)) (vec!0,vec!0) vec
+
 -- | Scalar product
 scProd :: Vector (Data Float) -> Vector (Data Float) -> Data Float
 scProd a b = sum (zipWith (*) a b)
